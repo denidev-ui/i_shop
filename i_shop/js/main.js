ca8ed930d223a8fce95da591b43f9f990898b0f5
denideveloper <burmeidan@gmail.com>
2021-02-09 16:56:13 +0200

    HEADER

--- i_shop/js/main.js
+++ i_shop/js/main.js
@@ -1,1 +1,11 @@
 console.log('work!')
+
+async function getData() {
+    let data = await fetch(
+        'https://audiopl-5219a.firebaseio.com/Like.json'
+    );
+    let main = await data.json();
+    console.log(main)
+}
+
+getData();
