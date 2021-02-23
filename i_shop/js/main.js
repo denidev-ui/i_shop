4d0ea8a720309dbd4553f77961b4a5bf4ea5fc75
denideveloper <burmeidan@gmail.com>
2021-02-23 13:28:36 +0200

    Two pages mobile

--- i_shop/js/main.js
+++ i_shop/js/main.js
@@ -1,11 +1,17 @@
-console.log('work!')
+var menuButton = document.querySelector('.mobile_menu_logo');
+var mobileMenu = document.querySelector('.mobile_menu');
+var header = document.querySelector('header')
+var state = menuButton.getAttribute('state')
 
-async function getData() {
-    let data = await fetch(
-        'https://audiopl-5219a.firebaseio.com/Like.json'
-    );
-    let main = await data.json();
-    console.log(main)
+menuButton.onclick = function() {
+    if (state == 'close') {
+        mobileMenu.style.transform = 'translateY(0)'
+        state = 'open'
+        header.style.boxShadow = 'none'
+    }
+    else if (state == 'open') {
+        mobileMenu.style.transform = 'translateY(-230px)'
+        state = 'close'
+        header.style.boxShadow = '-1px 15px 60px 2px rgba(170, 169, 169, 0.23)'
+    }
 }
-
-getData();
